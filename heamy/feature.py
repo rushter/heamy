9cd450b2823ebf2713bf8fbb7404a11cc07e09bf
rushter <me@rushter.com>
2016-07-14 14:04:30 +0300

    Add target features

--- heamy/feature.py
+++ heamy/feature.py
@@ -1,5 +1,8 @@
 # coding:utf-8
+from functools import partial
+
 import pandas as pd
+import numpy as np
 
 
 def onehot_features(train, test, features, full=False, sparse=False, dummy_na=True):
@@ -80,3 +83,68 @@ def factorize(train, test, features, na_value=-9999, full=False, sort=True):
             test[column] = test[column].replace(-1, na_value)
 
     return train, test
+
+
+def woe(df, feature_name, target_name):
+    """Calculate weight of evidence.
+
+    Parameters
+    ----------
+    df: Dataframe
+    feature_name: str
+        Column name to encode.
+    target_name: str
+        Target column name.
+
+    Returns
+    -------
+    Series
+
+    """
+
+    def group_woe(group):
+        event = float(group.sum())
+        non_event = group.shape[0] - event
+
+        rel_event = event / event_total
+        rel_non_event = non_event / non_event_total
+
+        return np.log(rel_non_event / rel_event) * 100
+
+    if df[target_name].nunique() > 2:
+        raise ValueError('Target column should be binary (1/0).')
+
+    event_total = float(df[df[target_name] == 1.0].shape[0])
+    non_event_total = float(df.shape[0] - event_total)
+
+    woe_vals = df.groupby(feature_name)[target_name].transform(group_woe)
+    return woe_vals
+
+
+def mean_target(df, feature_name, target_name, C=None):
+    """Mean target.
+    Original idea: Stanislav Semenov
+
+    Parameters
+    ----------
+    C : float, default None
+        Regularization coefficient. The higher, the more conservative result.
+        The optimal value lies between 10 and 50 depending on the data.
+    feature_name : str
+    target_name : str
+    df: DataFrame
+
+    Returns
+    -------
+    Series
+    """
+
+    def group_mean(group):
+        group_size = float(group.shape[0])
+        if C is None:
+            return (group.mean() * group_size + global_mean) / group_size
+        else:
+            return (group.mean() * group_size + global_mean * C) / (group_size + C)
+
+    global_mean = df[target_name].mean()
+    return df.groupby(feature_name)[target_name].transform(group_mean)
